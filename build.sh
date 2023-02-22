70994460c2203568e78e9899744e7efd8be91ed9
WeiMing <3793512+BruceYang-yeu@users.noreply.github.com>
2023-02-22 11:20:07 +0800

    opt: fix compile
    update build.sh, fix compile error.
    
    erros message:
    #error This file requires compiler and library support for the ISO C++ 2011 standard. This support is currently experimental, and must be enabled with the -std=c++11 or -std=gnu++11 compiler options.
     #error This file requires compiler and library support for the

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
-g++ -O3 -c cpuid_x86.cpp
-g++ -O3 -o cpuid_gen cpuid_x86.o
+g++ -O3 -std=c++11 -c cpuid_x86.cpp
+g++ -O3 -std=c++11 -o cpuid_gen cpuid_x86.o
 ./cpuid_gen
 sh build_kernel.sh
 
-g++ -O3 -c table.cpp
-g++ -O3 -c smtl.cpp
-g++ -O3 -c cpubm_x86.cpp
-g++ -O3 -c cpufp_x86.cpp
+g++ -O3 -std=c++11 -c table.cpp
+g++ -O3 -std=c++11 -c smtl.cpp
+g++ -O3 -std=c++11 -c cpubm_x86.cpp
+g++ -O3 -std=c++11 -c cpufp_x86.cpp
 
 sh link.sh
