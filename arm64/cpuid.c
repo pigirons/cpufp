b9ce03a34bf1bac0c00f211bd712d655fff4e41b
pigirons <pigirons@hotmail.com>
2023-12-09 10:13:54 +0000

    add cpuid detect for armv8 asimd, hp, dp support.

--- arm64/cpuid.c
+++ arm64/cpuid.c
@@ -1,31 +1,56 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <asm/hwcap.h>
+#include <sys/auxv.h>
 
 typedef enum
 {
     _ASIMD_    = 0x1,
     _ASIMD_HP_ = 0x2,
-    _ASIMD_DP_ = 0x3,
+    _ASIMD_DP_ = 0x4,
 } x64_simd_t;
 
-static unsigned int feat = 0;
+#define GET_INTEVAL(FEAT, AT, SIZE) (((FEAT) >> (AT)) & ((0x1 << (SIZE)) - 1))
 
-#define BIT_TEST(bit_map, pos) (((bit_map) & (0x1 << (pos))) ? 1 : 0)
-#define SET_FEAT(feat_mask) { feat |= (feat_mask); }
-
-static void cpuid_init()
+static uint64_t _GET_ID_AA64PFR0_EL1()
 {
-    if (feat != 0)
-    {
-        return;
-    }
+    uint64_t __cpuid;
+    asm("mrs %0, ID_AA64PFR0_EL1" : "=r" (__cpuid));
+    return __cpuid;
+}
+
+static uint64_t _GET_ID_AA64ISAR0_EL1()
+{
+    uint64_t __cpuid;
+    asm("mrs %0, ID_AA64ISAR0_EL1" : "=r" (__cpuid));
+    return __cpuid;
 }
 
 int main()
 {
-    cpuid_init();
+	if (!(getauxval(AT_HWCAP) & HWCAP_CPUID))
+    {
+		return 1;
+	}
+    uint64_t feat = 0;
 
-    printf("_ASIMD_\n");
+    feat = _GET_ID_AA64ISAR0_EL1();
+    if (GET_INTEVAL(feat, 44, 4) == 0x1)
+    {
+        printf("_ASIMD_DP_\n");
+    }
 
-    return 0;
+    feat = _GET_ID_AA64PFR0_EL1();
+    if (GET_INTEVAL(feat, 20, 4) == 0)
+    {
+        printf("_ASIMD_\n");
+    }
+    else if (GET_INTEVAL(feat, 20, 4) == 0x1)
+    {
+        printf("_ASIMD_HP_\n");
+        printf("_ASIMD_\n");
+    }
+
+	return 0;
 }
 
