f18728bd85d41b8a1bf4709ac033e3e35a318f9d
pigirons <pigirons@hotmail.com>
2023-12-12 11:33:11 +0000

    add hwcap preprocess detection.

--- arm64/cpuid.c
+++ arm64/cpuid.c
@@ -7,31 +7,41 @@ int main()
 {
     uint64_t hwcaps = getauxval(AT_HWCAP);
 
+#ifdef HWCAP2_I8MM
     if (hwcaps & HWCAP2_I8MM)
     {
         printf("_I8MM_\n");
     }
+#endif
 
+#ifdef HWCAP2_BF16
     if (hwcaps & HWCAP2_BF16)
     {
         printf("_BF16_\n");
     }
+#endif
 
+#ifdef HWCAP_ASIMDDP
     if (hwcaps & HWCAP_ASIMDDP)
     {
         printf("_ASIMD_DP_\n");
     }
+#endif
 
+#ifdef HWCAP_ASIMDHP
     if (hwcaps & HWCAP_ASIMDHP)
     {
         printf("_ASIMD_HP_\n");
     }
-    
+#endif
+
+#ifdef HWCAP_ASIMD
     if (hwcaps & HWCAP_ASIMD)
     {
         printf("_ASIMD_\n");
     }
+#endif
 
-	return 0;
+    return 0;
 }
 
